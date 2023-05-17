77f1fccb4b63a1bc1f9e1af8d9be5e12ccf44fc3
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-05-17 10:15:18 -0400

    Add employee dto

---
+++ src/main/java/com/example/storemanagement/employee/EmployeeDTO.java
@@ -0,0 +1,87 @@
+package com.example.storemanagement.employee;
+
+public abstract class EmployeeDTO {
+
+    private String role;
+    private String nationalid;
+    private String phonenumber;
+    private String email;
+    private String dob;
+    private String address;
+    private String lastname;
+    private String firstname;
+    private int id;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getNationalid() {
+        return nationalid;
+    }
+
+    public void setNationalid(String nationalid) {
+        this.nationalid = nationalid;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
